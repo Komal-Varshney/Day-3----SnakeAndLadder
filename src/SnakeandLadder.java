ba1c63fec83fe7060b594fb732a7095885d00949
Komal-Varshney <varshneyk578@gmail.com>
2025-01-02 02:36:53 +0530

    Added: Dice roll and positions

--- src/SnakeandLadder.java
+++ src/SnakeandLadder.java
@@ -1,44 +1,18 @@
 import java.util.Random;
 
 public class SnakeandLadder {
-//    public static void main(String[] args) {
-//        Random random = new Random();
-//        int dieRoll = random.nextInt(6) + 1; // Generates a number between 1 and 6
-//        System.out.println("Die rolled: " + dieRoll);
-//    }
-
-//    public static void main(String[] args) {
-//        Random random = new Random();
-//        int dieRoll = random.nextInt(6) + 1;
-//        int option = random.nextInt(3); // 0: No Play, 1: Ladder, 2: Snake
-//        int position = 0;
-//
-//        switch (option) {
-//            case 0: // No Play
-//                System.out.println("No Play. Current position: " + position);
-//                break;
-//            case 1: // Ladder
-//                position += dieRoll;
-//                System.out.println("Ladder! Move ahead to position: " + position);
-//                break;
-//            case 2: // Snake
-//                position -= dieRoll;
-//                position = Math.max(position, 0);
-//                System.out.println("Snake! Move back to position: " + position);
-//                break;
-//        }
-//    }
-
     public static void main(String[] args) {
         Random random = new Random();
         int position = 0;
+        int diceRollCount = 0;
 
         while (position < 100) {
             int dieRoll = random.nextInt(6) + 1;
+            diceRollCount++;
             int option = random.nextInt(3);
 
             if (position + dieRoll > 100) {
-                continue; // Skip if the position exceeds 100
+                continue;
             }
 
             switch (option) {
@@ -52,8 +26,8 @@ public class SnakeandLadder {
                     position = Math.max(position, 0);
                     break;
             }
-            System.out.println("Current position: " + position);
+            System.out.println("Roll " + diceRollCount + ": Position = " + position);
         }
-        System.out.println("Congratulations! You reached position 100.");
+        System.out.println("Total dice rolls: " + diceRollCount);
     }
 }
