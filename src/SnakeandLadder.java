a81888523190a0efdc4cdcf393bb37c8510a2b39
Komal-Varshney <varshneyk578@gmail.com>
2025-01-02 02:31:46 +0530

    Added: winning position

--- src/SnakeandLadder.java
+++ src/SnakeandLadder.java
@@ -7,25 +7,50 @@ public class SnakeandLadder {
 //        System.out.println("Die rolled: " + dieRoll);
 //    }
 
+//    public static void main(String[] args) {
+//        Random random = new Random();
+//        int dieRoll = random.nextInt(6) + 1;
+//        int option = random.nextInt(3); // 0: No Play, 1: Ladder, 2: Snake
+//        int position = 0;
+//
+//        switch (option) {
+//            case 0: // No Play
+//                System.out.println("No Play. Current position: " + position);
+//                break;
+//            case 1: // Ladder
+//                position += dieRoll;
+//                System.out.println("Ladder! Move ahead to position: " + position);
+//                break;
+//            case 2: // Snake
+//                position -= dieRoll;
+//                position = Math.max(position, 0);
+//                System.out.println("Snake! Move back to position: " + position);
+//                break;
+//        }
+//    }
+
     public static void main(String[] args) {
         Random random = new Random();
-        int dieRoll = random.nextInt(6) + 1;
-        int option = random.nextInt(3); // 0: No Play, 1: Ladder, 2: Snake
         int position = 0;
 
-        switch (option) {
-            case 0: // No Play
-                System.out.println("No Play. Current position: " + position);
-                break;
-            case 1: // Ladder
-                position += dieRoll;
-                System.out.println("Ladder! Move ahead to position: " + position);
-                break;
-            case 2: // Snake
-                position -= dieRoll;
-                position = Math.max(position, 0);
-                System.out.println("Snake! Move back to position: " + position);
-                break;
+        while (position < 100) {
+            int dieRoll = random.nextInt(6) + 1;
+            int option = random.nextInt(3);
+
+            switch (option) {
+                case 0: // No Play
+                    break;
+                case 1: // Ladder
+                    position += dieRoll;
+                    break;
+                case 2: // Snake
+                    position -= dieRoll;
+                    position = Math.max(position, 0);
+                    break;
+            }
+            System.out.println("Current position: " + position);
         }
+        System.out.println("Congratulations! You reached position 100.");
     }
+
 }
